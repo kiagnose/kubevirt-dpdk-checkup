51590180878fc262aebd486b5eef982da5f166a1
Orel Misan <omisan@redhat.com>
2023-01-23 12:09:22 +0200

    checkup, setup: Create a VirtualMachineInstance object
    Create the VirtualMachineInstance object to be used
    as a test subject.
    
    A follow-up commit will also wait for the VMI to boot.
    
    The VMI object is created with a random suffix, in order to
    enable execution of multiple checkup instances simultaneously.
    
    Signed-off-by: Orel Misan <omisan@redhat.com>

--- pkg/internal/checkup/checkup.go
+++ pkg/internal/checkup/checkup.go
@@ -21,18 +21,46 @@ package checkup
 
 import (
 	"context"
+	"fmt"
 
+	k8srand "k8s.io/apimachinery/pkg/util/rand"
+
+	kvcorev1 "kubevirt.io/api/core/v1"
+
+	"github.com/kiagnose/kubevirt-dpdk-checkup/pkg/internal/checkup/vmi"
+	"github.com/kiagnose/kubevirt-dpdk-checkup/pkg/internal/config"
 	"github.com/kiagnose/kubevirt-dpdk-checkup/pkg/internal/status"
 )
 
-type Checkup struct {
+type kubeVirtVMIClient interface {
+	CreateVirtualMachineInstance(ctx context.Context,
+		namespace string,
+		vmi *kvcorev1.VirtualMachineInstance) (*kvcorev1.VirtualMachineInstance, error)
 }
 
-func New() *Checkup {
-	return &Checkup{}
+type Checkup struct {
+	client    kubeVirtVMIClient
+	namespace string
+	vmi       *kvcorev1.VirtualMachineInstance
+}
+
+const VMINamePrefix = "dpdk-vmi"
+
+func New(client kubeVirtVMIClient, namespace string, checkupConfig config.Config) *Checkup {
+	return &Checkup{
+		client:    client,
+		namespace: namespace,
+		vmi:       newDPDKVMI(checkupConfig),
+	}
 }
 
 func (c *Checkup) Setup(ctx context.Context) error {
+	createdVMI, err := c.client.CreateVirtualMachineInstance(ctx, c.namespace, c.vmi)
+	if err != nil {
+		return err
+	}
+	c.vmi = createdVMI
+
 	return nil
 }
 
@@ -46,4 +74,53 @@ func (c *Checkup) Teardown(ctx context.Context) error {
 
 func (c *Checkup) Results() status.Results {
 	return status.Results{}
+}
+
+func newDPDKVMI(checkupConfig config.Config) *kvcorev1.VirtualMachineInstance {
+	const (
+		CPUSocketsCount   = 1
+		CPUCoresCount     = 4
+		CPUTreadsCount    = 2
+		rootDiskName      = "rootdisk"
+		cloudInitDiskName = "cloudinitdisk"
+		userData          = `#cloud-config
+user: cloud-user
+password: 0tli-pxem-xknu
+chpasswd:
+  expire: false`
+
+		eastNetworkName   = "nic-east"
+		eastNICPCIAddress = "0000:06:00.0"
+		westNetworkName   = "nic-west"
+		westNICPCIAddress = "0000:07:00.0"
+
+		terminationGracePeriodSeconds = 180
+	)
+
+	return vmi.New(randomizeName(VMINamePrefix),
+		vmi.WithoutCRIOCPULoadBalancing(),
+		vmi.WithoutCRIOCPUQuota(),
+		vmi.WithoutCRIOIRQLoadBalancing(),
+		vmi.WithDedicatedCPU(CPUSocketsCount, CPUCoresCount, CPUTreadsCount),
+		vmi.WithSRIOVInterface(eastNetworkName, checkupConfig.DPDKEastMacAddress.String(), eastNICPCIAddress),
+		vmi.WithMultusNetwork(eastNetworkName, checkupConfig.NetworkAttachmentDefinitionName),
+		vmi.WithSRIOVInterface(westNetworkName, checkupConfig.DPDKWestMacAddress.String(), westNICPCIAddress),
+		vmi.WithMultusNetwork(westNetworkName, checkupConfig.NetworkAttachmentDefinitionName),
+		vmi.WithNetworkInterfaceMultiQueue(),
+		vmi.WithRandomNumberGenerator(),
+		vmi.WithHugePages(),
+		vmi.WithMemoryRequest("8Gi"),
+		vmi.WithTerminationGracePeriodSeconds(terminationGracePeriodSeconds),
+		vmi.WithNodeSelector(checkupConfig.DPDKNodeLabelSelector),
+		vmi.WithPVCVolume(rootDiskName, "rhel8-yummy-gorilla"),
+		vmi.WithVirtIODisk(rootDiskName),
+		vmi.WithCloudInitNoCloudVolume(cloudInitDiskName, userData),
+		vmi.WithVirtIODisk(cloudInitDiskName),
+	)
+}
+
+func randomizeName(prefix string) string {
+	const randomStringLen = 5
+
+	return fmt.Sprintf("%s-%s", prefix, k8srand.String(randomStringLen))
 }
