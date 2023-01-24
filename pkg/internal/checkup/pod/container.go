ec5f0d7fb11d85dae9eda73326ade87a5c5f22f8
Ram Lavi <ralavi@redhat.com>
2023-01-24 15:15:31 +0200

    Introduce the pod package
    This package exposes the `NewPod()` and `NewPodContainer()` function and
    their option functions.
    These will help build the Traffic Generator Pod object that will be used
    by the checkup.
    
    Signed-off-by: Ram Lavi <ralavi@redhat.com>

---
+++ pkg/internal/checkup/pod/container.go
@@ -0,0 +1,118 @@
+/*
+ * This file is part of the kiagnose project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Copyright 2023 Red Hat, Inc.
+ *
+ */
+
+package pod
+
+import (
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
+)
+
+// ContainerOption represents a container action that enables an option.
+type ContainerOption func(pod *corev1.Container)
+
+const ContainerDiskImage = "quay.io/schseba/trex:2.87"
+
+func NewPodContainer(name string, opts ...ContainerOption) *corev1.Container {
+	container := &corev1.Container{
+		Name: name,
+	}
+
+	for _, f := range opts {
+		f(container)
+	}
+	return container
+}
+
+func WithContainerImage(image string) ContainerOption {
+	return func(container *corev1.Container) {
+		container.Image = image
+		container.ImagePullPolicy = corev1.PullAlways
+	}
+}
+
+func WithContainerCommand(command []string) ContainerOption {
+	return func(container *corev1.Container) {
+		container.Command = command
+	}
+}
+
+func WithContainerSecurityContext(sc *corev1.SecurityContext) ContainerOption {
+	return func(container *corev1.Container) {
+		container.SecurityContext = sc
+	}
+}
+
+func WithContainerEnvVars(envVars map[string]string) ContainerOption {
+	return func(container *corev1.Container) {
+		for key, val := range envVars {
+			envVar := corev1.EnvVar{
+				Name:  key,
+				Value: val,
+			}
+			container.Env = append(container.Env, envVar)
+		}
+	}
+}
+
+func WithContainerCPUsStrict(cpusNumString string) ContainerOption {
+	return func(container *corev1.Container) {
+		if container.Resources.Requests == nil {
+			container.Resources.Requests = make(corev1.ResourceList)
+		}
+		if container.Resources.Limits == nil {
+			container.Resources.Limits = make(corev1.ResourceList)
+		}
+		numCpusQuantity := resource.MustParse(cpusNumString)
+		container.Resources.Requests[corev1.ResourceCPU] = numCpusQuantity
+		container.Resources.Limits[corev1.ResourceCPU] = numCpusQuantity
+	}
+}
+
+func WithContainerHugepagesResources(hugepagesNumString string) ContainerOption {
+	return func(container *corev1.Container) {
+		const memorySize = "1Gi"
+		memoryQuantity := resource.MustParse(memorySize)
+		container.Resources.Requests[corev1.ResourceMemory] = memoryQuantity
+		container.Resources.Limits[corev1.ResourceMemory] = memoryQuantity
+
+		hugepagesQuantity := resource.MustParse(hugepagesNumString)
+		container.Resources.Requests[corev1.ResourceHugePagesPrefix+memorySize] = hugepagesQuantity
+		container.Resources.Limits[corev1.ResourceHugePagesPrefix+memorySize] = hugepagesQuantity
+	}
+}
+
+func WithContainerHugepagesVolumeMount() ContainerOption {
+	return func(container *corev1.Container) {
+		container.VolumeMounts = append(container.VolumeMounts, corev1.VolumeMount{
+			Name:      "hugepages",
+			MountPath: "/mnt/huge",
+		})
+	}
+}
+
+func NewSecurityContext(user int64, allowPrivilegeEscalation bool, addCapabilities []corev1.Capability) *corev1.SecurityContext {
+	return &corev1.SecurityContext{
+		AllowPrivilegeEscalation: pointer(allowPrivilegeEscalation),
+		Capabilities: &corev1.Capabilities{
+			Add: addCapabilities,
+		},
+		RunAsUser: pointer(user),
+	}
+}
---
+++ pkg/internal/checkup/pod/spec.go
@@ -0,0 +1,151 @@
+/*
+ * This file is part of the kiagnose project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Copyright 2023 Red Hat, Inc.
+ *
+ */
+
+package pod
+
+import (
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+)
+
+// PodOption represents a pod action that enables an option.
+type PodOption func(pod *corev1.Pod)
+
+const (
+	// Based on annotation names from:
+	// https://github.com/cri-o/cri-o/blob/fa0fa5de1c17ddd7b6fcdbc030b6b571ce37e643/pkg/annotations/annotations.go
+
+	// CRIOCPULoadBalancingAnnotation indicates that load balancing should be disabled for CPUs used by the container
+	CRIOCPULoadBalancingAnnotation = "cpu-load-balancing.crio.io"
+
+	// CRIOCPUQuotaAnnotation indicates that CPU quota should be disabled for CPUs used by the container
+	CRIOCPUQuotaAnnotation = "cpu-quota.crio.io"
+
+	// CRIOIRQLoadBalancingAnnotation indicates that IRQ load balancing should be disabled for CPUs used by the container
+	CRIOIRQLoadBalancingAnnotation = "irq-load-balancing.crio.io"
+)
+
+const Disable = "disable"
+
+func NewPod(name string, opts ...PodOption) *corev1.Pod {
+	pod := &corev1.Pod{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: name,
+		},
+	}
+
+	for _, f := range opts {
+		f(pod)
+	}
+
+	return pod
+}
+
+func pointer[T any](v T) *T {
+	return &v
+}
+
+func WithOwnerReference(ownerName, ownerUID string) PodOption {
+	return func(pod *corev1.Pod) {
+		if ownerUID != "" && ownerName != "" {
+			pod.ObjectMeta.OwnerReferences = append(pod.ObjectMeta.OwnerReferences, metav1.OwnerReference{
+				APIVersion: "v1",
+				Kind:       "Pod",
+				Name:       ownerName,
+				UID:        types.UID(ownerUID),
+			})
+		}
+	}
+}
+
+func WithLabels(labels map[string]string) PodOption {
+	return func(pod *corev1.Pod) {
+		if pod.Labels == nil {
+			pod.Labels = map[string]string{}
+		}
+		for key, value := range labels {
+			pod.Labels[key] = value
+		}
+	}
+}
+
+func WithHugepagesVolume() PodOption {
+	return func(pod *corev1.Pod) {
+		pod.Spec.Volumes = append(pod.Spec.Volumes,
+			corev1.Volume{
+				Name: "hugepages",
+				VolumeSource: corev1.VolumeSource{
+					EmptyDir: &corev1.EmptyDirVolumeSource{
+						Medium: "HugePages",
+					},
+				},
+			})
+	}
+}
+
+func WithoutCRIOCPULoadBalancing() PodOption {
+	return func(pod *corev1.Pod) {
+		if pod.ObjectMeta.Annotations == nil {
+			pod.ObjectMeta.Annotations = map[string]string{}
+		}
+
+		pod.ObjectMeta.Annotations[CRIOCPULoadBalancingAnnotation] = Disable
+	}
+}
+
+func WithoutCRIOCPUQuota() PodOption {
+	return func(pod *corev1.Pod) {
+		if pod.ObjectMeta.Annotations == nil {
+			pod.ObjectMeta.Annotations = map[string]string{}
+		}
+
+		pod.ObjectMeta.Annotations[CRIOCPUQuotaAnnotation] = Disable
+	}
+}
+
+func WithoutCRIOIRQLoadBalancing() PodOption {
+	return func(pod *corev1.Pod) {
+		if pod.ObjectMeta.Annotations == nil {
+			pod.ObjectMeta.Annotations = map[string]string{}
+		}
+
+		pod.ObjectMeta.Annotations[CRIOIRQLoadBalancingAnnotation] = Disable
+	}
+}
+
+func WithNodeSelector(nodeName string) PodOption {
+	return func(pod *corev1.Pod) {
+		if nodeName == "" {
+			return
+		}
+
+		if pod.Spec.NodeSelector == nil {
+			pod.Spec.NodeSelector = map[string]string{}
+		}
+
+		pod.Spec.NodeSelector[corev1.LabelHostname] = nodeName
+	}
+}
+
+func WithPodContainer(container *corev1.Container) PodOption {
+	return func(pod *corev1.Pod) {
+		pod.Spec.Containers = append(pod.Spec.Containers, *container)
+	}
+}
