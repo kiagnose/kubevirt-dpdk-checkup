34798e7e1dd53a85ddce055cd31a88e53b0a5930
Orel Misan <omisan@redhat.com>
2023-02-02 14:25:00 +0200

    executor: Run testpmd in the VMI
    1. Build the testpmd command-line arguments
    2. Wait for it to successfully init.
    3. Start it.
    
    Signed-off-by: Orel Misan <omisan@redhat.com>

--- pkg/internal/checkup/executor/executor.go
+++ pkg/internal/checkup/executor/executor.go
@@ -22,7 +22,11 @@ package executor
 import (
 	"context"
 	"fmt"
+	"log"
+	"strings"
 	"time"
+
+	expect "github.com/google/goexpect"
 
 	"kubevirt.io/client-go/kubecli"
 
@@ -64,5 +68,59 @@ func (e Executor) Execute(ctx context.Context, vmiName string) (status.Results, 
 		return status.Results{}, fmt.Errorf("failed to login to VMI \"%s/%s\": %w", e.namespace, vmiName, err)
 	}
 
+	log.Printf("Starting testpmd in VMI...")
+	if err := e.runTestpmd(vmiName); err != nil {
+		return status.Results{}, err
+	}
+
 	return status.Results{}, nil
+}
+
+func (e Executor) runTestpmd(vmiName string) error {
+	const batchTimeout = 30 * time.Second
+
+	const testpmdPromt = "testpmd> "
+
+	testpmdCmd := buildTestpmdCmd(e.vmiEastNICPCIAddress, e.vmiWestNICPCIAddress, e.vmiEastMACAddress, e.vmiWestMACAddress)
+
+	resp, err := console.SafeExpectBatchWithResponse(e.client, e.namespace, vmiName,
+		[]expect.Batcher{
+			&expect.BSnd{S: testpmdCmd + "\n"},
+			&expect.BExp{R: testpmdPromt},
+			&expect.BSnd{S: "start" + "\n"},
+			&expect.BExp{R: testpmdPromt},
+		},
+		batchTimeout,
+	)
+
+	if err != nil {
+		return err
+	}
+
+	log.Printf("%v", resp)
+
+	return nil
+}
+
+func buildTestpmdCmd(vmiEastNICPCIAddress, vmiWestNICPCIAddress, vmiEastMACAddress, vmiWestMACAddress string) string {
+	const (
+		cpuList       = "0-7"
+		numberOfCores = 7
+	)
+
+	sb := strings.Builder{}
+	sb.WriteString("dpdk-testpmd ")
+	sb.WriteString(fmt.Sprintf("-l %s ", cpuList))
+	sb.WriteString(fmt.Sprintf("-a %s ", vmiEastNICPCIAddress))
+	sb.WriteString(fmt.Sprintf("-a %s ", vmiWestNICPCIAddress))
+	sb.WriteString("-- ")
+	sb.WriteString("-i ")
+	sb.WriteString(fmt.Sprintf("--nb-cores=%d ", numberOfCores))
+	sb.WriteString("--rxd=2048 ")
+	sb.WriteString("--txd=2048 ")
+	sb.WriteString("--forward-mode=mac ")
+	sb.WriteString(fmt.Sprintf("--eth-peer=0,%s ", vmiEastMACAddress))
+	sb.WriteString(fmt.Sprintf("--eth-peer=1,%s", vmiWestMACAddress))
+
+	return sb.String()
 }
