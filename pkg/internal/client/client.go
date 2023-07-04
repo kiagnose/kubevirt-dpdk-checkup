2c4c45679454dc0492191dd680acf695bce7913b
Orel Misan <omisan@redhat.com>
2023-07-04 18:34:25 +0300

    Client: Cleanup pod and NAD
    As there are no usages for the Pod and NetworkAttachmentDefinition
    operations, remove them from the client.
    
    Signed-off-by: Orel Misan <omisan@redhat.com>

--- pkg/internal/client/client.go
+++ pkg/internal/client/client.go
@@ -20,33 +20,17 @@
 package client
 
 import (
-	"bytes"
 	"context"
 	"time"
 
-	networkv1 "github.com/k8snetworkplumbingwg/network-attachment-definition-client/pkg/apis/k8s.cni.cncf.io/v1"
-	netattdefclient "github.com/k8snetworkplumbingwg/network-attachment-definition-client/pkg/client/clientset/versioned/typed/k8s.cni.cncf.io/v1"
-
-	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/client-go/kubernetes/scheme"
 	"k8s.io/client-go/rest"
-	"k8s.io/client-go/tools/remotecommand"
-
 	kvcorev1 "kubevirt.io/api/core/v1"
 	"kubevirt.io/client-go/kubecli"
 )
 
 type Client struct {
 	kubecli.KubevirtClient
-	netattdefclient.K8sCniCncfIoV1Interface
-	config *rest.Config
-}
-
-type executeWrapper struct {
-	stdout string
-	stderr string
-	err    error
 }
 
 func New() (*Client, error) {
@@ -60,12 +44,7 @@ func New() (*Client, error) {
 		return nil, err
 	}
 
-	cniClient, err := netattdefclient.NewForConfig(config)
-	if err != nil {
-		return nil, err
-	}
-
-	return &Client{client, cniClient, config}, nil
+	return &Client{client}, nil
 }
 
 func (c *Client) CreateVirtualMachineInstance(ctx context.Context,
@@ -87,81 +66,4 @@ func (c *Client) VMISerialConsole(namespace, name string, timeout time.Duration)
 		name,
 		&kubecli.SerialConsoleOptions{ConnectionTimeout: timeout},
 	)
-}
-
-func (c *Client) CreatePod(ctx context.Context, namespace string, pod *corev1.Pod) (*corev1.Pod, error) {
-	return c.KubevirtClient.CoreV1().Pods(namespace).Create(ctx, pod, metav1.CreateOptions{})
-}
-
-func (c *Client) GetPod(ctx context.Context, namespace, name string) (*corev1.Pod, error) {
-	return c.KubevirtClient.CoreV1().Pods(namespace).Get(ctx, name, metav1.GetOptions{})
-}
-
-func (c *Client) DeletePod(ctx context.Context, namespace, name string) error {
-	return c.KubevirtClient.CoreV1().Pods(namespace).Delete(ctx, name, metav1.DeleteOptions{})
-}
-
-func (c *Client) ExecuteCommandOnPod(ctx context.Context,
-	namespace, name, containerName string,
-	command []string) (stdout, stderr string, err error) {
-	resultCh := make(chan executeWrapper, 1)
-
-	go func() {
-		var (
-			stdoutBuf bytes.Buffer
-			stderrBuf bytes.Buffer
-		)
-		options := remotecommand.StreamOptions{
-			Stdout: &stdoutBuf,
-			Stderr: &stderrBuf,
-			Tty:    false,
-		}
-
-		err = executeCommandOnPodWithOptions(c.KubevirtClient, c.config, namespace, name, containerName, command, options)
-		stdout = stdoutBuf.String()
-		stderr = stderrBuf.String()
-		resultCh <- executeWrapper{stdout, stderr, err}
-	}()
-
-	select {
-	case result := <-resultCh:
-		return result.stdout, result.stderr, result.err
-	case <-ctx.Done():
-		return stdout, stderr, ctx.Err()
-	}
-}
-
-func (c *Client) GetNetworkAttachmentDefinition(
-	ctx context.Context,
-	namespace, name string) (*networkv1.NetworkAttachmentDefinition, error) {
-	return c.K8sCniCncfIoV1Interface.NetworkAttachmentDefinitions(namespace).Get(ctx, name, metav1.GetOptions{})
-}
-
-func executeCommandOnPodWithOptions(virtCli kubecli.KubevirtClient, clientConfig *rest.Config,
-	namespace, name, containerName string,
-	command []string,
-	options remotecommand.StreamOptions) error {
-	req := virtCli.CoreV1().RESTClient().Post().
-		Resource("pods").
-		Name(name).
-		Namespace(namespace).
-		SubResource("exec").
-		Param("container", containerName)
-
-	req.VersionedParams(&corev1.PodExecOptions{
-		Container: containerName,
-		Command:   command,
-		Stdin:     false,
-		Stdout:    true,
-		Stderr:    true,
-		TTY:       false,
-	}, scheme.ParameterCodec)
-
-	executor, err := remotecommand.NewSPDYExecutor(clientConfig, "POST", req.URL())
-	if err != nil {
-		return err
-	}
-
-	//nolint
-	return executor.Stream(options)
 }
