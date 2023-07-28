b4dbfa09ae3a84b3cdcb9867cb1ffb1b2d7af577
Ram Lavi <ralavi@redhat.com>
2023-07-28 12:23:37 +0300

    console, executor: Move LoginToCentOS to a method of Expecter object
    The LoginToCentOS is a basic function of the console. it starts of the
    initial login and basic configuration logic, using all the internal
    params of the expecter object. It does so using one spawned console
    instead of opening multiple ones. All of the above suggest it should
    be a method of the object itself.
    Moving to be a method of the expecter object.
    
    Signed-off-by: Ram Lavi <ralavi@redhat.com>

--- pkg/internal/checkup/executor/console/login.go
+++ pkg/internal/checkup/executor/console/login.go
@@ -29,14 +29,13 @@ import (
 	"google.golang.org/grpc/codes"
 )
 
-func LoginToCentOS(serialConsoleClient vmiSerialConsoleClient, vmiNamespace, vmiName, username, password string) error {
+func (e Expecter) LoginToCentOS(username, password string) error {
 	const (
 		connectionTimeout = 10 * time.Second
 		promptTimeout     = 5 * time.Second
 	)
 
-	expecter := NewExpecter(serialConsoleClient, vmiNamespace, vmiName)
-	genExpect, err := expecter.spawnConsole(connectionTimeout)
+	genExpect, err := e.spawnConsole(connectionTimeout)
 	if err != nil {
 		return err
 	}
@@ -49,7 +48,7 @@ func LoginToCentOS(serialConsoleClient vmiSerialConsoleClient, vmiNamespace, vmi
 
 	// Do not login, if we already logged in
 	loggedInPromptRegex := fmt.Sprintf(
-		`(\[%s@(localhost|centos|%s) ~\]\$ |\[root@(localhost|centos|%s) centos\]\# )`, username, vmiName, vmiName,
+		`(\[%s@(localhost|centos|%s) ~\]\$ |\[root@(localhost|centos|%s) centos\]\# )`, username, e.vmiName, e.vmiName,
 	)
 	b := []expect.Batcher{
 		&expect.BSnd{S: "\n"},
@@ -67,7 +66,7 @@ func LoginToCentOS(serialConsoleClient vmiSerialConsoleClient, vmiNamespace, vmi
 			&expect.Case{
 				// Using only "login: " would match things like "Last failed login: Tue Jun  9 22:25:30 UTC 2020 on ttyS0"
 				// and in case the VM's did not get hostname form DHCP server try the default hostname
-				R:  regexp.MustCompile(fmt.Sprintf(`(localhost|centos|%s) login: `, vmiName)),
+				R:  regexp.MustCompile(fmt.Sprintf(`(localhost|centos|%s) login: `, e.vmiName)),
 				S:  fmt.Sprintf("%s\n", username),
 				T:  expect.Next(),
 				Rt: 10,
--- pkg/internal/checkup/executor/executor.go
+++ pkg/internal/checkup/executor/executor.go
@@ -72,12 +72,14 @@ func New(client vmiSerialConsoleClient, namespace string, cfg config.Config) Exe
 
 func (e Executor) Execute(ctx context.Context, vmiUnderTestName, trafficGenVMIName string) (status.Results, error) {
 	log.Printf("Login to VMI under test...")
-	if err := console.LoginToCentOS(e.vmiSerialClient, e.namespace, vmiUnderTestName, e.vmiUsername, e.vmiPassword); err != nil {
+	vmiUnderTestConsoleExpecter := console.NewExpecter(e.vmiSerialClient, e.namespace, vmiUnderTestName)
+	if err := vmiUnderTestConsoleExpecter.LoginToCentOS(e.vmiUsername, e.vmiPassword); err != nil {
 		return status.Results{}, fmt.Errorf("failed to login to VMI \"%s/%s\": %w", e.namespace, vmiUnderTestName, err)
 	}
 
 	log.Printf("Login to traffic generator...")
-	if err := console.LoginToCentOS(e.vmiSerialClient, e.namespace, trafficGenVMIName, e.vmiUsername, e.vmiPassword); err != nil {
+	trafficGenConsoleExpecter := console.NewExpecter(e.vmiSerialClient, e.namespace, trafficGenVMIName)
+	if err := trafficGenConsoleExpecter.LoginToCentOS(e.vmiUsername, e.vmiPassword); err != nil {
 		return status.Results{}, fmt.Errorf("failed to login to VMI \"%s/%s\": %w", e.namespace, trafficGenVMIName, err)
 	}
 
